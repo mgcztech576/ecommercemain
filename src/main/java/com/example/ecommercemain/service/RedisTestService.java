2d4f82c1a7bcfcf4bf04123e336946c2810b5161
mgcztech576 <127584002+mgcztech576@users.noreply.github.com>
2023-10-06 20:14:13 +0900

    aaaa

---
+++ src/main/java/com/example/ecommercemain/service/RedisTestService.java
@@ -0,0 +1,16 @@
+package com.example.ecommercemain.service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.TimeUnit;
+@Slf4j @Service @RequiredArgsConstructor
+public class RedisTestService {
+    private final RedissonClient redissonClient;
+    public String getLock(String id) {
+        RLock lock = redissonClient.getLock("sampleLock");
+        try {boolean isLock = lock.tryLock(1, 5, TimeUnit.SECONDS);
+            if(!isLock){log.error("Failed");return "Lock failed";}
+    }catch (Exception e){log.error("Redis lock failed");} return "Lock success";}
+}
