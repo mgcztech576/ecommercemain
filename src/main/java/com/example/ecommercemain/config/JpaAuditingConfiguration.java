0a73ac9795b0adadd4f9f0a1aba4844a334c96ec
mgcztech576 <127584002+mgcztech576@users.noreply.github.com>
2023-10-10 16:52:04 +0900

    aaaa

---
+++ src/main/java/com/example/ecommercemain/config/JpaAuditingConfiguration.java
@@ -0,0 +1,2 @@
+package com.example.ecommercemain.config;
+public class JpaAuditingConfiguration { }
--- src/main/java/com/example/ecommercemain/config/LocalRedisConfig.java
+++ src/main/java/com/example/ecommercemain/config/LocalRedisConfig.java
@@ -1,17 +1,17 @@
-//package com.example.ecommercemain.config;
-//import jakarta.annotation.PostConstruct;
-//import jakarta.annotation.PreDestroy;
-////import lombok.Value;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import redis.embedded.RedisServer;
-//@Configuration
-//public class LocalRedisConfig {
-//    @Value("${spring.redis.port}")
-//    private int redisPort;
-//    private RedisServer redisServer;
-//    @PostConstruct public void startRedis(){
-//        redisServer =new RedisServer(redisPort);
-//        redisServer.start();}
-//    @PreDestroy public void stopRedis(){
-//        if(redisServer!=null) redisServer.stop();}}
+package com.example.ecommercemain.config;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+//import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+@Configuration
+public class LocalRedisConfig {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    private RedisServer redisServer;
+    @PostConstruct public void startRedis(){
+        redisServer =new RedisServer(redisPort);
+        redisServer.start();}
+    @PreDestroy public void stopRedis(){
+        if(redisServer!=null) redisServer.stop();}}
